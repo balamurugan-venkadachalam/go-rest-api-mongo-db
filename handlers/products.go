2b6b552a07069db5fe720850c0b8e1e1c5d69acc
Balamurugan Venkadachalam <engan.balamurugan@gmail.com>
2021-10-03 19:31:08 +1300

    delete update

--- handlers/products.go
+++ handlers/products.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"example.com/m/v2/dbiface"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"gopkg.in/go-playground/validator.v9"
@@ -14,6 +15,11 @@ import (
 var (
 	v = validator.New()
 )
+
+type Response struct {
+	input interface{}
+	error string `json:"error"`
+}
 
 type Product struct {
 	ID          primitive.ObjectID `json:"_id,omitempty" bson:"id,omitempty"`
@@ -50,7 +56,7 @@ func (p *ProductHandler) CreateProducts(c echo.Context) error {
 	for _, product := range products {
 		err := c.Validate(product)
 		if err != nil {
-			log.Printf("Unable to validate product %+v %v", product, err)
+			log.Printf("Unable to validate product %+v error -> %v", product, err)
 			return err
 		}
 	}
@@ -61,6 +67,64 @@ func (p *ProductHandler) CreateProducts(c echo.Context) error {
 
 	return c.JSON(http.StatusCreated, IDs)
 
+}
+
+func (p *ProductHandler) DeleteProducts(c echo.Context) error {
+	return nil
+}
+
+func (p *ProductHandler) DeleteProduct(c echo.Context) error {
+	_, err :=deleteProduct(context.Background(), c.Param("id"), p.Col)
+	if err != nil {
+		log.Printf("Unable to delete product %v", err)
+		return err
+	}
+	return c.JSON(http.StatusOK, nil)
+}
+
+
+func (p *ProductHandler) GetProduct(c echo.Context) error {
+	product, err := findProduct(context.Background(), c.Param("id"),p.Col)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, product)
+}
+
+
+
+func (p *ProductHandler) GetProducts(c echo.Context) error {
+	return nil
+}
+
+func (p *ProductHandler) PutProduct(c echo.Context) error {
+	return nil
+}
+
+func deleteProduct(ctx context.Context, param string, col *mongo.Collection) (bool, error){
+	docID, err := primitive.ObjectIDFromHex(param)
+	if err != nil {
+		return  false, err
+	}
+	_, err = col.DeleteOne(ctx, bson.M{"_id": docID})
+	if err != nil {
+		return false, err
+	}
+	return true, err
+}
+
+func findProduct(ctx context.Context, param string, collection dbiface.CollectionAPI) (Product, error) {
+	var product Product
+	docID, err := primitive.ObjectIDFromHex(param)
+	if err != nil {
+		return product, err
+	}
+	res := collection.FindOne(ctx, bson.M{"_id": docID})
+	err = res.Decode(&product)
+	if err != nil {
+		return product, err
+	}
+	return product, nil
 }
 
 func insertProducts(ctx context.Context, products []Product, collection dbiface.CollectionAPI) ([]interface{}, error) {
