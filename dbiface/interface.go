70edcfb3b3c0913a88aeb5f01d03cb901a69010f
Balamurugan Venkadachalam <engan.balamurugan@gmail.com>
2021-10-03 19:33:15 +1300

    Revert "delete update"
    This reverts commit 2b6b552a07069db5fe720850c0b8e1e1c5d69acc.

--- dbiface/interface.go
+++ dbiface/interface.go
@@ -9,7 +9,5 @@ import (
 type (
 	CollectionAPI interface {
 		InsertOne(ctx context.Context, document interface{}, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error)
-		FindOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) *mongo.SingleResult
-		DeleteOne(ctx context.Context, filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error)
 	}
 )
